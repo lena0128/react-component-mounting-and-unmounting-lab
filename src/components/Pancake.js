ab48394b8b5ee629679fce8dde1f36200bc75265
Lena Hou <lenahou2013@gmail.com>
2021-11-21 12:44:09 -0500

    commit tree 741d64f80ac060704daf9c5f8a2bb09ac1762dc5

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -12,7 +12,15 @@ class Pancake extends React.Component {
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
 
+  componentDidMount(){
+    this.startInterval()
+  }
+
   // TODO: create a componentWillUnmount() which will clear the interval
+componentWillUnmount(){
+  this.cleanUpInterval()
+}
+
 
   updateCounter = () => {
     this.setState({
@@ -64,6 +72,7 @@ class Pancake extends React.Component {
     return (
       <div className={`Pancake --${status}`}>
         <div className="Pancake__content">
+          
           <p>I am a pancake.</p>
           <p>
             Time cooked on {`${firstSide ? "first" : "second"}`} side:{" "}
